fa9c11fb2b30a9d944567bf311c3c61aec2e06fd
SathanBERNARD <nathan.bernard.etudiant@gmail.com>
2022-06-03 23:13:59 +0200

    able to render successive maps

--- display.c
+++ display.c
@@ -5,20 +5,29 @@
 
 char spacetochar(Space *s);
 
-
 void display(Map* map)
 {
+  printf("\e[1;1H\e[2J");
+  if (*map->comment)
+    printf("--> %s <--\n\n", map->comment);
+  else
+    printf("\n\n");
+
   int maxx = map->size.x, maxy = map->size.y;
+  char* ligne = emalloc(sizeof(char)*maxy+1); //emalloc met à 0
   for (int x = 0; x < maxx; x++) {
-    char* ligne = emalloc(sizeof(char)*maxx+1); //emalloc met à 0
     for (int y = 0; y < maxy; y++) {
       ligne[y] = spacetochar(&map->grid[x][y]);
     }
     printf("%s\n", ligne);
-    free(ligne); //TODO opti
+    for (int i = 0; i < maxy+1; i++) ligne[i] = 0;
   }
-  printf("MAP n°%u from %s\n", map->id, map->author);
-  printf("%s\n", map->comment);
+  if (*map->author)
+    printf("\nMap n°%u from %s\n", map->id, map->author);
+  else
+    printf("\nMap n°%u \n", map->id);
+
+  free(ligne);
 }
 
 char spacetochar(Space *s){
@@ -44,6 +53,6 @@ char spacetochar(Space *s){
     case EMPTY:
       break;
   }
-  if(c == '!') error("CA MARCHE PAS!!!");  //TODO meilleur msg
+  if(c == '!') error("spacetochar : invalid input space");
   return c;
 }
