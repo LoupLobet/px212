7938d8c647a18fac3e7dfc130c7b2ef9c6976c37
Loup Lobet <loup@Loups-MacBook-Pro.local>
2022-06-03 21:13:45 +0200

    fixes multiple comment error in loadmap() + bad getmapsize() design

--- loader.c
+++ loader.c
@@ -7,19 +7,11 @@
 #include "util.h"
 #include "sokoban.h"
 
-#define NO_PLAYER { -1, -1 };
-
 enum { AUTHOR, COMMENT, LEVEL, PASS };
 
 union value {
 	int i;
 	char *s;
-};
-
-struct lvl {
-	unsigned int line;
-	struct lvl *next;
-	struct lvl *prev;
 };
 
 struct comment {
@@ -31,15 +23,16 @@ static struct comment	*parsecomment(FILE *);
 static Pair	 getmapsize(FILE *fp);
 static int	 gotolevel(FILE *, int);
 
-struct lvl *table;
-
 int
 main(int argc, char *argv[])
 {
 	int i, j;
 	Map *m;
 
-	m = loadmap("levels.lvl", 1);
+	m = loadmap("levels.lvl", 82);
+	if (m == NULL) {
+		error("could not load map");
+	}
 
 	for (j = 0; j < m->size.y; j++) {
 		for (i = 0; i < m->size.x; i++) {
@@ -55,8 +48,8 @@ main(int argc, char *argv[])
 Map *
 loadmap(char *file, int n)
 {
-	int i;
 	int c;
+	int i;
 	int x, y;
 	struct comment *cmt;
 	FILE *fp;
@@ -71,75 +64,63 @@ loadmap(char *file, int n)
 		return NULL;
 	}
 	m = emalloc(sizeof(Map));
-	m->size = getmapsize(fp);
 	m->id = n;
-	m->player = (Pair)NO_PLAYER;
-	m->grid = emalloc(sizeof(Space *) * m->size.x);
-	for (i = 0; i < m->size.x; i++)
-		m->grid[i] = emalloc(sizeof(Space) * m->size.y);
+	m->player = (Pair){ -1, -1 };
+
+	/* catch extra comments */
+	while ((c = fgetc(fp)) != EOF && c == ';') {
+		cmt = parsecomment(fp);
+		switch (cmt->op) {
+		case AUTHOR:
+			m->author = estrdup(cmt->val.s);
+			break;
+		case COMMENT:
+			m->comment = estrdup(cmt->val.s);
+			break;
+		case LEVEL:
+			/* empty grid, error */
+			warning("empty map: %d", n);
+			return NULL;
+		case PASS:
+			break;
+		}
+		free(cmt->val.s);
+		free(cmt);
+	}
 	/* fill the grid */
 	x = 0;
 	y = 0;
-	while ((c = fgetc(fp)) != EOF) {
+	m->size = getmapsize(fp);
+	m->grid = emalloc(sizeof(Space) * m->size.x);
+	for (i = 0; i < m->size.x; i++)
+		m->grid[i] = emalloc(sizeof(Space) * m->size.y);
+	while ((c = fgetc(fp)) != EOF && c != ';') {
 		if (c == '\n') {
 			x = 0;
 			y++;
 			continue;
 		}
-		if (c == ';') {
-			if (x == 0 && y == 0) {
-				/* additional comment before map grid */
-				cmt = parsecomment(fp);
-				switch (cmt->op) {
-				case AUTHOR:
-					m->author = estrdup(cmt->val.s);
-					break;
-				case COMMENT:
-					m->comment = estrdup(cmt->val.s);
-					break;
-				case LEVEL:
-					/* empty map, error */
-					warning("empty map: %d", n);
-					return NULL;
-				case PASS:
-					break;
-				}
-				free(cmt);
-			} else {
-				/* map grid not empty, new comment signals the end of the map */
-				break;
-			}
-		} else {
-			switch (c) {
-			case FLOOR:
-				m->grid[x][y].type = FLOOR;
-				m->grid[x][y].content = EMPTY;
-				break;
-			case WALL:
-				m->grid[x][y].type = WALL;
-				m->grid[x][y].content = EMPTY;
-				break;
-			case TARGET:
-				m->grid[x][y].type = TARGET;
-				m->grid[x][y].content = EMPTY;
-				break;
-			case PLAYER:
-				m->grid[x][y].type = FLOOR;
-				m->grid[x][y].content = PLAYER;
-				m->player = (Pair){ x, y };
-				break;
-			case BOX:
-				m->grid[x][y].type = FLOOR;
-				m->grid[x][y].content = BOX;
-				break;
-			default:
-				warning("unknown character %c, in map: %d", c, n);
-				return NULL;
-			}
-			x++;
+		switch (c) {
+		case FLOOR:
+		case WALL:
+		case TARGET:
+			m->grid[x][y].type = c;
+			m->grid[x][y].content = EMPTY;
+			break;
+		case PLAYER:
+			m->player = (Pair){ x, y };
+			/* fallthrough */
+		case BOX:
+			m->grid[x][y].type = FLOOR;
+			m->grid[x][y].content = c;
+			break;
+		default:
+			warning("unknown character %c, in map: %d", c, n);
+			return NULL;
 		}
+		x++;
 	}
-	if (m->player.x == -1 && m->player.y == -1) { // voir pour une egalite plus propre avec macro ?
+	if (m->player.x == -1 && m->player.y == -1) {
 		warning("no player in map: %d");
 		return NULL;
 	}
@@ -158,15 +139,18 @@ getmapsize(FILE *fp)
 	len = 0;
 	n = 0;
 	while ((c = fgetc(fp)) != EOF && c != ';') {
-		if (c == '\n' && size.x < len) {
-			size.x = len;
+		if (c == '\n') {
 			size.y++;
-		}
-		len++;
+			if (size.x < len)
+				size.x = len;
+			len = 0;
+		} else
+			len++;
 		n++;
+
 	}
 	/* keep fp unchanged */
-	fseek(fp, -n, SEEK_CUR);
+	fseek(fp, -(n + 1), SEEK_CUR);
 	return size;
 }
 
@@ -185,8 +169,9 @@ parsecomment(FILE *fp)
 	opsize = 16;
 	op = emalloc(opsize);
 	for (i = 0; (c = fgetc(fp)) >= 0 && c != ' '; i++) {
-		if (c == EOF || c == '\n')
+		if (c == EOF || c == '\n') {
 			return cmt;
+		}
 		if (i > opsize - 2)
 			op = erealloc(op, opsize * 2);
 		op[i] = c;
