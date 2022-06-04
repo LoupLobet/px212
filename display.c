87b8dee80d5b729214a23113d09648b57cb85401
SathanBERNARD <nathan.bernard.etudiant@gmail.com>
2022-06-04 19:44:34 +0200

    display cursor, more resilient to empty comment/author

--- display.c
+++ display.c
@@ -3,26 +3,34 @@
 #include "display.h"
 #include "util.h"
 
-char spacetochar(Space *s);
+static char spacetochar(Space *s);
 
 void display(Map* map)
 {
+  // clear
   printf("\e[1;1H\e[2J");
-  if (*map->comment)
+  // print comment if exist
+  if (map->comment && *map->comment)
     printf("--> %s <--\n\n", map->comment);
   else
     printf("\n\n");
-
+  // display map
   int maxx = map->size.x, maxy = map->size.y;
-  char* ligne = emalloc(sizeof(char)*maxy+1); //emalloc met à 0
-  for (int x = 0; x < maxx; x++) {
-    for (int y = 0; y < maxy; y++) {
-      ligne[y] = spacetochar(&map->grid[x][y]);
+  char* ligne = emalloc(sizeof(char)*maxx+1); //emalloc met à 0
+  for (int y = 0; y < maxy; y++) {
+    for (int x = 0; x < maxx; x++) {
+      ligne[x] = spacetochar(&map->grid[x][y]);
     }
+    //display player and cursor
+    if (map->cursor.y == y)
+      ligne[map->cursor.x] = 'O';
+    if (map->player.y == y)
+      ligne[map->player.x] = '@'; //TODO faire un choix entre map->player et map->grid[][].content
     printf("%s\n", ligne);
-    for (int i = 0; i < maxy+1; i++) ligne[i] = 0;
+    for (int i = 0; i < maxx+1; i++) ligne[i] = 0;
   }
-  if (*map->author)
+  // print author if exist and map id
+  if (map->author && *map->author)
     printf("\nMap n°%u from %s\n", map->id, map->author);
   else
     printf("\nMap n°%u \n", map->id);
@@ -30,7 +38,7 @@ void display(Map* map)
   free(ligne);
 }
 
-char spacetochar(Space *s){
+static char spacetochar(Space *s){
   char c = '!';
   switch (s->type) {
     case WALL:
@@ -44,7 +52,7 @@ char spacetochar(Space *s){
       break;
   }
   switch (s->content) {
-    case PLAYER:
+    case PLAYER:  //TODO faire un choix
       c = '@';
       break;
     case BOX:
@@ -53,6 +61,6 @@ char spacetochar(Space *s){
     case EMPTY:
       break;
   }
-  if(c == '!') error("spacetochar : invalid input space");
+  // if(c == '!') error("spacetochar : invalid input space");  TODO regler ce pb
   return c;
 }
