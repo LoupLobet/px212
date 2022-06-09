a35c23ae424cf3c6545c0fcbc244d4fa0c70866b
SathanBERNARD <nathan.bernard.etudiant@gmail.com>
2022-06-09 10:07:35 +0200

    doxygen commented

--- display.c
+++ display.c
@@ -17,10 +17,15 @@ static Pair cursor0 = (Pair){1, 3};
 
 static void displaymap(Map* map, int leftmargin);
 static void displayheader(Map* map);
-static void displaycursor();
+static void updatecursor();
 static char spacetochar(Space *s);
 
-
+/**
+ * @brief Display the game page (map, header, footer).
+ * @param map map of the game
+ * @param mvnb strokes number
+ * @return void
+ */
 void display(Map* map, int mvnb)
 {
 	// clear
@@ -51,14 +56,22 @@ void display(Map* map, int mvnb)
 	printf("\n\n");
 }
 
-
+/**
+ * @brief Display a text at a fixed location : the bottom of the game page.
+ * @param s string to display
+ * @return void
+ */
 void displaystr(char *s) {
 	printf("\e[1A\e[2K");
 	printf("%s\n", s);
-	displaycursor();
+	updatecursor();
 }
 
-
+/**
+ * @brief Display a warning at a fixed location : the bottom of the game page.
+ * @param s warning text to display
+ * @return void
+ */
 void displaywarning(char *fmt, ...) {
 	va_list ap;
 
@@ -68,10 +81,15 @@ void displaywarning(char *fmt, ...) {
 	(void)vprintf(fmt, ap);
 	va_end(ap);
 	putchar('\n');
-	displaycursor();
+	updatecursor();
 }
 
-
+/**
+ * @brief Display the map left aligned with a left margin.
+ * @param map map to display
+ * @param leftmargin left margin, 0: no margin
+ * @return void
+ */
 static void displaymap(Map* map, int leftmargin)	{
 	int maxx = map->size.x, maxy = map->size.y;
 
@@ -93,7 +111,11 @@ static void displaymap(Map* map, int leftmargin)	{
 	free(ligne);
 }
 
-
+/**
+ * @brief Display the header and compute the data necessary to center the map and the header.
+ * @param map map displayed after the header
+ * @return void
+ */
 static void displayheader(Map* map)	{
 	int size = map->size.x;
 	if (size > (23-4)){
@@ -112,9 +134,13 @@ static void displayheader(Map* map)	{
 }
 
 
-/// --- CURSOR --- \\\
-
-static void displaycursor(){
+/// --- CURSOR --- ///
+/**
+ * @brief update the cursor according to the global position 'cursorpos'.
+ * @param void
+ * @return void
+ */
+static void updatecursor(void){
 	int column0 = cursor0.x, line0 = cursor0.y;
 	int x = cursorpos.x, y = cursorpos.y;
 	if(x != -1 && y != -1){
@@ -125,7 +151,12 @@ static void displaycursor(){
 	} else fflush(stdout);
 }
 
-
+/// --- CURSOR --- ///
+/**
+ * @brief convert a Space to the char to be displayed.
+ * @param s Space to convert
+ * @return char char representing the Space
+ */
 static char spacetochar(Space *s){
 	// cas sol (défault)
 	char c = ' ';
@@ -138,21 +169,36 @@ static char spacetochar(Space *s){
 	return c;
 }
 
-
+/**
+ * @brief set cursor position ((Pair){-1, -1} to hide it), while forcing it in the map boundaries.
+ * @param map the map in wich the cursor evolves
+ * @param pos the wished position of the cursor
+ * @return void
+ */
 void setcursor(Map *map, Pair pos){
 	int px = pos.x, py = pos.y;
 	Pair ms = map->size;
 	if(px == -1 && py == -1) cursorpos = pos;
 	else cursorpos = (Pair){MIN(MAX(pos.x, 0), ms.x-1), MIN(MAX(pos.y, 0), ms.y-1)};
-	displaycursor();
+	updatecursor();
 }
 
-
+/**
+ * @brief move cursor, while forcing it in the map boundaries.
+ * @param map the map in wich the cursor evolves
+ * @param pos the wished move to apply to the cursor
+ * @return void
+ */
 void movecursor(Map *map, Pair mvt){
 	setcursor(map, (Pair){cursorpos.x + mvt.x, cursorpos.y + mvt.y});
 }
 
-
-Pair getcursor() {
+/**
+ * @brief give the cursor position ((Pair){-1, -1} if hidden).
+ * @param void
+ * @return Pair{x,y} cursor position on the grid ((Pair){-1, -1} if hidden)
+ */
+Pair getcursor(void) {
+	if (cursorpos.x ==-1 && cursorpos.y == -1) return (Pair){-1, -1};
 	return (Pair){cursorpos.x - cursor0.x, cursorpos.y - cursor0.y};
 }
