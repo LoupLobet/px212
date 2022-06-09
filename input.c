caa86cea4f0e64ab89a99ea7684183978cefa5ad
Loup Lobet <loup@eduroam-044207.grenet.fr>
2022-06-09 09:23:20 +0200

    review

--- input.c
+++ input.c
@@ -1,59 +1,64 @@
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <unistd.h>
+
 #include "input.h"
+#include "termios.h"
 
-
-int io(void) {
-
-  int key;
-
-  char r[MAXSIZE];
-	int c;  
-
-	if ((c=read(0,r,3)) == - 1 ) return false; 
-
-	switch (r[0]) {
-	case 27 :  
-      if ((c==3) && (r[1]==91)){
-        key = (r[2]-64);
-        switch (key) {
-    
-        case UP: return 'U'; break;
-        case DOWN: return 'D'; break;
-        case LEFT: return 'L'; break;
-        case RIGHT:return 'R'; break;
-        default:  return 'P'; break;
-        }
-      }  
-      else if (c == 1 && r[0] == 27)
-        return 27; 
-	  else
-	  	return false;
-      break; 
-	default: return r[0]; break; 
-	}
-				
-          
-      
-}
-
-
-
-
-
-int configureTerminal()
+int
+configureTerminal(void)
 {
 	struct termios new;
+
 	if (tcgetattr(0,&new)==-1) {
-      		perror("tcgetattr");
+		perror("tcgetattr");
 		return -1;
-    	}
-	new.c_lflag &= ~(ICANON); // Met le terminal en mode canonique.
-	new.c_lflag &= ~(ECHO);   // les touches tapées ne s'inscriront plus dans le terminal
+	}
+	new.c_lflag &= ~(ICANON); /* canonical termianal */
+	new.c_lflag &= ~(ECHO);   /* do not display keyboard entries anymore */
 	new.c_cc[VMIN]=1;
 	new.c_cc[VTIME]=0;
 	if (tcsetattr(0,TCSANOW,&new)==-1) {
-      		perror("tcsetattr");
-		return false;
-    	}
-	return true;
+		perror("tcsetattr");
+		return 0;
+	}
+	return 1;
 }
 
+int
+io(void) {
+	char r[MAXSIZE];
+	int c;
+	int key;
+
+	if ((c = read(0,r,3)) == - 1 )
+		return 0;
+	switch (r[0]) {
+	case 27 :
+		if (c == 3 && r[1] == 91){
+			key = (r[2] - 64);
+			switch (key) {
+			case UP:
+				return 'U';
+			case DOWN:
+				return 'D';
+			case LEFT:
+				return 'L';
+			case RIGHT:
+				return 'R';
+			default:
+				return 'P';
+			}
+		} else if (c == 1 && r[0] == 27)
+			return 27;
+		else
+			return 0;
+		break;
+	default:
+		return r[0];
+		break;
+	}
+}
--- input.h
+++ input.h
@@ -1,14 +1,8 @@
-#include <termios.h>
-#include <unistd.h> 
-#include <stdio.h>
-#include <stdbool.h>
-
-
-#define MAXSIZE 10 
+#define MAXSIZE 10
 #define UP 1
 #define LEFT 4
 #define RIGHT 3
 #define DOWN 2
 
-int io(void);
 int configureTerminal();
+int io(void);
