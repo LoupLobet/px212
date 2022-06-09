13f2f845dd8fe6a2c90138730c1a1258d1877ea9
Loup Lobet <loup@eduroam-044207.grenet.fr>
2022-06-09 09:23:36 +0200

    review

--- move.c
+++ move.c
@@ -1,45 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "display.h"
+#include "input.h"
 #include "move.h"
-
-
-/**
- * @brief This function allow the user to move the player in the map.
- * For that we verify first if we can move with canwemove() and then
- * we manage each case when we can do a movement.
- * Different cases are : we can move with a box in front of us. So in
- * this case, we move the player and the box and we specify that a box
- * moved to save it + the move in a stack (s).
- * The other case is : we can move and in front of us they are nothing.
- * So , we simply move the player and specify that no box move to save
- * it in the stack s.
- * @param m Map
- * @param move Move that we want to do
- * @param s Stack of movement
- * @return int 1 if the player has been moved, 0 otherwise.
- */
-int
-move (Map *m, Pair move,Stack **s)
-{
-    Pair targ = { (*m).player.x + move.x, (*m).player.y };
-    Pair player = { (*m).player.x, (*m).player.y };
-    Pair behind = { (*m).player.x + 2 * move.x, (*m).player.y + 2 * move.y };
-    int boxmoved;
-
-    if (canmove(m,move)) {
-        if ((*m).grid[targ.x][targ.x].content == BOX) {
-        	boxmoved = 1;
-            (*m).grid[behind][behind].content = BOX;
-        } else {
-            boxmoved = 0;
-        (*m).grid[player.x][player.y].content = EMPTY;
-		player.x = targ.x;
-        player.y = targ.y;
-		(*m).grid[targ.x][targ.y].content = PLAYER;
-		if (s != NULL)
-			pushstack(s, move, boxmoved);
-        return 1;
-    }
-    return 0;
-}
+#include "sokoban.h"
+#include "util.h"
 
 /**
  * @brief This function is used to verify if we can do the movement
@@ -60,12 +26,135 @@ canmove(Map *m, Pair move)
 	Pair targ = { m->player.x + move.x, m->player.y + move.y };
 	Pair behind = { m->player.x + 2 * move.x, m->player.y + 2 * move.y };
 
-	if (m->grid[targ][targ].type != WALL || m->grid[targ][targ].content == BOX &&
-	    m->grid[behind][behind].type != WALL)
+	if (m->grid[targ.x][targ.y].content == EMPTY ||
+		m->grid[targ.x][targ.y].content == BOX &&
+	    m->grid[behind.x][behind.y].content == EMPTY)
 	{
 		return 1;
 	} else
 		return 0;
+}
+
+/**
+ * @brief This function allows the user to move the cursor placed
+ * first at the player position when he pass to cursor mode.This
+ * function is the beginning for advanced functions when we want
+ * to specify a destination to go.
+ * @param m Map
+ * @return void
+ */
+void
+cursormove(Map *m)
+{
+	char move;
+	setcursor(m, (Pair){m->player.x, m->player.y});
+	while ((move = io()) != 27) {
+		switch (move) {
+		case 'U':
+			movecursor(m, (Pair){ 0, -1 });
+			break;
+		case 'D':
+			movecursor(m, (Pair){ 0, 1 });
+			break;
+		case 'L':
+			movecursor(m, (Pair){ -1, 0 });
+			break;
+		case 'R':
+			movecursor(m, (Pair){ 1, 0 });
+			break;
+		default:
+			displaywarning("Invalid move");
+		}
+		displaycursor();
+	}
+}
+
+/**
+ * @brief This function can be used to show the solution to the user
+ * or for the Artificial Intelligence by doing automatically the
+ * movement that are save in the stack input. For that, if input is
+ * not null , we simply pop the last element of the stack so the first
+ * move to do and we use move to do it. Of course, we do a move so we
+ * increase the counter because the function return it. We do a sleep
+ * of 1 second to show to the user each move independantly.
+ * @param input Stack of movement that we want to do
+ * @param m Map
+ * @param output Stack of movement of the level
+ * @param stroke Number of strokes
+ * @return void
+ */
+void
+execstack(Stack *input, Map *m, Stack **output)
+{
+	Stack pop;
+
+	if (input == NULL)
+		displaywarning("No move to do");
+	else {
+		while (!popstack(&input, &pop))
+			move(m, pop.move, output);
+	}
+}
+
+/**
+ * @brief This function allow the user to move the player in the map.
+ * For that we verify first if we can move with canwemove() and then
+ * we manage each case when we can do a movement.
+ * Different cases are : we can move with a box in front of us. So in
+ * this case, we move the player and the box and we specify that a box
+ * moved to save it + the move in a stack (s).
+ * The other case is : we can move and in front of us they are nothing.
+ * So , we simply move the player and specify that no box move to save
+ * it in the stack s.
+ * @param m Map
+ * @param move Move that we want to do
+ * @param s Stack of movement
+ * @return int 1 if the player has been moved, 0 otherwise.
+ */
+int
+move (Map *m, Pair move,Stack **s)
+{
+	Pair targ = { m->player.x + move.x, m->player.y + move.y };
+    int boxmoved;
+
+    if (canmove(m, move)) {
+        if (m->grid[targ.x][targ.y].content == BOX) {
+            m->grid[targ.x + move.x][targ.y + move.y].content = BOX;
+            boxmoved = 1;
+        } else
+            boxmoved = 0;
+        m->grid[m->player.x][m->player.y].content = EMPTY;
+
+		m->player.x = targ.x;
+        m->player.y = targ.y;
+        m->grid[targ.x][targ.y].content = PLAYER;
+		if (s != NULL)
+			pushstack(s, move, boxmoved);
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief This function pop the last element of the stack s in the stack
+ * pop to use it after. We return 1 if they are a problem and 0 else.
+ * @param s stack
+ * @param pop poped element
+ * @return int 0 if the stack has been poped (wasn't empty), 1 otherwise.
+ */
+int
+popstack(Stack **s, Stack *pop)
+{
+	Stack *p;
+
+	if (*s == NULL)
+		return 1;
+	*pop = **s;
+	p = *s;
+	*s = p->prev;
+	free(p);
+
+	return 0;
 }
 
 /**
@@ -99,28 +188,6 @@ Stack
 }
 
 /**
- * @brief This function pop the last element of the stack s in the stack
- * pop to use it after. We return 1 if they are a problem and 0 else.
- * @param s stack
- * @param pop poped element
- * @return int 0 if the stack has been poped (wasn't empty), 1 otherwise.
- */
-int
-popstack(Stack **s, Stack *pop)
-{
-	Stack *p;
-
-	if (*s == NULL)
-		return 1;
-	*pop = **s;
-	p = *s;
-	*s = p->prev;
-	free(p);
-
-	return 0;
-}
-
-/**
  * @brief This function allows the user to undo his last movement.
  * For that, we use the stack s where we save all the movement and
  * we second stack p. We pop the last element of s and we place it
@@ -136,80 +203,18 @@ int
 undomove(Stack **s, Map *m)
 {
 	Stack pop;
-	Pair player = { m->player.x, m->player.y };
 
 	if (popstack(s,&pop)) {
 		displaywarning("No move to undo !");
 		return 1;
 	}
-	m->grid[player.x][player.y].content = EMPTY;
-	player.x -= pop.move.x;
-	player.y -= pop.move.y;
-	m->grid[player.x][player.y].content = PLAYER;
-	if (p.boxmoved == 1) {
-		m->grid[player.x + pop.move.x][player.y + pop.move.y].content = BOX;
-		m->grid[player.x + 2*(pop.move.x)][player.y + 2*(pop.move.y)].content = EMPTY;
+	m->grid[m->player.x][m->player.y].content = EMPTY;
+	m->player.x -= pop.move.x;
+	m->player.y -= pop.move.y;
+	m->grid[m->player.x][m->player.y].content = PLAYER;
+	if (pop.boxmoved == 1) {
+		m->grid[m->player.x + pop.move.x][m->player.y + pop.move.y].content = BOX;
+		m->grid[m->player.x + 2*(pop.move.x)][m->player.y + 2*(pop.move.y)].content = EMPTY;
 	}
 	return 0;
-}
-
-/**
- * @brief This function can be used to show the solution to the user
- * or for the Artificial Intelligence by doing automatically the
- * movement that are save in the stack input. For that, if input is
- * not null , we simply pop the last element of the stack so the first
- * move to do and we use move to do it. Of course, we do a move so we
- * increase the counter because the function return it. We do a sleep
- * of 1 second to show to the user each move independantly.
- * @param input Stack of movement that we want to do
- * @param m Map
- * @param output Stack of movement of the level
- * @param stroke Number of strokes
- * @return void
- */
-void
-stack(Stack *input, Map *m, Stack **output)
-{
-	Stack pop;
-
-	if (input == NULL)
-		displaywarning("No move to do");
-	else {
-		while (!popstack(&input, &pop))
-			move(m, pop.move, output);
-	}
-}
-
-/**
- * @brief This function allows the user to move the cursor placed
- * first at the player position when he pass to cursor mode.This
- * function is the beginning for advanced functions when we want
- * to specify a destination to go.
- * @param m Map
- * @return void
- */
-void cursormove(Map *m)
-{
-	char move;
-	setcursor(m, (Pair){m->player.x, m->player.y});
-	displaycursor();
-	while ((move = io()) != 27) {
-		switch (move) {
-		case 'U':
-			movecursor(m, (Pair){ 0, -1 });
-			break;
-		case 'D':
-			movecursor(m, (Pair){ 0, 1 });
-			break;
-		case 'L':
-			movecursor(m, (Pair){ -1, 0 });
-			break;
-		case 'R':
-			movecursor(m, (Pair){ 1, 0 });
-			break;
-		default:
-			displaywarning("Invalid move");
-		}
-		displaycursor();
-	}
 }
--- move.h
+++ move.h
@@ -1,12 +1,9 @@
 #include "sokoban.h"
-#include "util.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-int	 move(Map *m, Pair move,Stack **s);
-Stack	*pushstack(Stack **s, Pair move, int boxmoved);
-int	 popstack(Stack **s, Stack *pop);
-int	 undomove(Stack **movement, Map *m);
 int	 canmove(Map *m, Pair move);
-void	 loadfromstack(Stack *input,Map *m,Stack *output);
-
+void	 cursormove(Map *m);
+void	 execstack(Stack *input,Map *m,Stack **output);
+int	 move(Map *m, Pair move,Stack **s);
+int	 popstack(Stack **s, Stack *pop);
+Stack	*pushstack(Stack **s, Pair move, int boxmoved);
+int	 undomove(Stack **movement, Map *m);
